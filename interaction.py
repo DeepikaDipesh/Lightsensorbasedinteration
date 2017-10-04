f4aa4416ec8eb6b56cd2b797770d7c4b21d3d5fb
deepiakdipesh <dipika_vinaykiya@yahoo.com>
2017-10-04 00:52:19 +0000

    tracked the omxp.pid

--- interaction.py
+++ interaction.py
@@ -27,16 +27,16 @@ movie4 = ("/home/pi/Downloads/sunny.mp4")
 movie5 = ("/home/pi/Downloads/black.mp4")
 
 
-def inputChng():
-    global omxp
-    global globalvar
-    print('globa')
-    print(globalvar)
-    if (globalvar==1):
-        print ('quit')
-        omxp.kill()
-        globalvar =0
-    print ('exits')
+##def inputChng():
+##    global omxp
+##    global globalvar
+##    print('globa')
+##    print(globalvar)
+##    if (globalvar==1):
+##        print ('quit')
+##        omxp.kill()
+##        globalvar =0
+##    print ('exits')
 
 globalvar =0
 
@@ -55,28 +55,28 @@ def playVideo (currentForecast):
     global omxp 
     if currentForecast == 'Clear':
         print ("The weather is clear!")
-        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie4],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
+        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie4],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False)
         print (omxp.pid)
     if currentForecast == 'Rain':
         print ("The weather is rain!")
-        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie3],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
+        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie3],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False)
         print (omxp.pid)
     if currentForecast == 'Clouds':
         print ("The weather is clouds!")
-        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie2],shell=False)
+        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie2],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False)
         print (omxp.pid)
     if currentForecast == 'Snow':
         print ("The weather is snow!")
-        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie1],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
+        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie1],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False)
     if currentForecast == 'Nothing':
-        print ("The weather is snow!")
-        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie5],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True)
+        print ("Black screen to be displayed")
+        omxp = subprocess.Popen(['omxplayer','--win','100 100 200 200',movie5],stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=False)
     return
 
-lightSensor1.when_dark=inputChng()
-lightSensor1.when_light=inputChng()
-lightSensor2.when_dark=inputChng()
-lightSensor2.when_light=inputChng()
+##lightSensor1.when_dark=inputChng()
+##lightSensor1.when_light=inputChng()
+##lightSensor2.when_dark=inputChng()
+##lightSensor2.when_light=inputChng()
 
 while True:
     print(lightSensor1.value)
@@ -84,7 +84,7 @@ while True:
         
     if lightSensor1.wait_for_dark(5):
         if (globalvar==1):
-            print ('quiting when sensor1 touched, I am killing' + oxmp.pid)
+            print ('quiting when sensor1 touched, I am killing' , omxp.pid)
             omxp.kill()
             globalvar =0
         print ("Sensor 1 is touched")
@@ -94,10 +94,9 @@ while True:
 
     if lightSensor2.wait_for_dark(5):
         if (globalvar==1):
-            print ('quiting when sensor2 touched, I am killing' + oxmp.pid)
+            print ('quiting when sensor2 touched, I am killing' , omxp.pid)
             omxp.kill()
             globalvar =0
-        #omxp.stdin.write('q')
         print ("Sensor 2 is touched")
         currentForecast = weatherinNextThreeHours['weather'][0]['main']
         playVideo (currentForecast)
@@ -106,11 +105,11 @@ while True:
     #Sensor 1 and 2 are dark (Use Case: Both are touched) 
     if (lightSensor1.light_detected and lightSensor2.light_detected):
         if (globalvar==1):
-            print ('quit when sensor2 is touched')
+            print ('quit when any sensor is touched', omxp.pid)
             omxp.kill()
             globalvar =0
         playVideo ("Nothing")
-        print ("Black screen to be displayed")
+        
 
 
 
